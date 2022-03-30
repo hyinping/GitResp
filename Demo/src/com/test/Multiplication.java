f3a33fd792d0c370837afb6403f7a391f07b72e7
hyinping <hyinping@vmware.com>
2022-03-30 16:08:25 +0800

    添九九乘法表程序

---
+++ Demo/src/com/test/Multiplication.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i < 10; i++){
+            for(int j = 1; j <=i; j++){
+
+                System.out.print( j + "*" + i + "=" + j*i + "\t");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
