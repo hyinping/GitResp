038f00b5ac3a20edff1dac9f4ef7e924f6a015c5
hyinping <hyinping@vmware.com>
2022-03-31 14:31:44 +0800

    添加了name，school, age, salary属性

--- Demo/src/com/test/PrintTest.java
+++ Demo/src/com/test/PrintTest.java
@@ -1,6 +1,11 @@
 package com.test;
 
 public class PrintTest {
+    private String name;
+    private String school;
+    private int age;
+    private double salary;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
